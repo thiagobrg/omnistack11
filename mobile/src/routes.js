74f4ee42b45f37b80f5baea1f2d62efb432062ce
Thiago Guimaraes <Thiago@Thiago.local>
2020-03-26 16:52:34 -0300

    mobile da aplicação

---
+++ mobile/src/routes.js
@@ -0,0 +1,22 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR'
+
+import React from 'react';
+import { NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const AppStack = createStackNavigator();
+
+import Incidents from './pages/Incidents/index';
+import Detail from './pages/Detail/index';
+
+export default function Routes(){
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{ headerShown: false} }>
+                <AppStack.Screen name="Incidents" component={Incidents} />
+                <AppStack.Screen name="Detail" component={Detail} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
